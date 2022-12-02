e5c2c5d927d286c4910d0fab5215069cff79cb7a
nicolesell <na23sell@siena.edu>
2022-12-01 22:51:59 -0500

    Update

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-//const moment = require('moment');
-
 function getLanguage() {  
     var list = document.getElementById('list');  
     var cur = list.value; 
     var text = list.options[list.selectedIndex].text;
     console.log("Selected language: " + text);
+    if(text = "en-US"){
+        moment.tz.setDefault("America/New_York");
+    }
+    if(text = "en-GB"){
+        moment.tz.setDefault("Europe/London");
+        console.log("hello");
+    }
+    if(text = "pt-BR")
+    {
+        moment.tz.setDefault("America/Belem");
+    }
 }
 
 var update = function() {
