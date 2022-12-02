83a595b402431d048fe7b2a31e8d51c741f68f82
nicolesell <na23sell@siena.edu>
2022-12-02 00:23:36 -0500

    Double Equals

--- script.js
+++ script.js
@@ -3,20 +3,26 @@ function getLanguage() {
     var cur = list.value; 
     var text = list.options[list.selectedIndex].text;
     console.log("Selected language: " + text);
-    if(text = "en-US"){
+    if(text == "en-US"){
         moment.tz.setDefault("America/New_York");
+        console.log(moment().format('h:mm:ss a'));
+        console.log(text);
     }
-    if(text = "en-GB"){
+    else if(text == "en-GB"){
         moment.tz.setDefault("Europe/London");
+        console.log(moment().format('h:mm:ss a'));
+        console.log(text);
     }
-    if(text = "pt-BR")
+    else if(text == "pt-BR")
     {
         moment.tz.setDefault("America/Belem");
+        console.log(moment().format('h:mm:ss a'));
+        console.log(text);
     }
 }
 
 var update = function() {
     document.getElementById("time")
-    .innerHTML = moment().format('h:mm:ss a ');
+    .innerHTML = moment().format('h:mm:ss a');
 }
 setInterval(update, 1000);
